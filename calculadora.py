d0683259eff7ccc21ee63e3533f6043564f55981
ferdelcorro <ferdelcorro@gmail.com>
2014-11-15 20:05:40 -0300

    un ejemplo de suma e identificar llamado de funcion y terminal

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,14 @@
-class calculadora:
+class calculadora():
     numero_1 = None
+    numero_2 = None
+    resultado = None
+
+    def suma(self):
+    	self.resultado = self.numero_1 + self.numero_2
+
+"""
+Este if sirve para distinguir a cuando se llama el archivo desde otro archivo
+o desde la terminal. En caso de ser desde la terminal entonces entra al if.
+"""
+if __name__ == '__main__':
+	calculadora = calculadora()
