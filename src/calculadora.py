da042a78350c8278484190c2737370a23c8d0362
Federico Ferri <federicoferriff@gmail.com>
2014-11-18 15:35:07 -0300

    Agrego la multiplicación y el método para ingresar los valores de entrada.

--- src/calculadora.py
+++ src/calculadora.py
@@ -12,22 +12,22 @@ class calculadora():
     Este método devuelve una tupla con dos números ingresados por consola.
     """
     def insertar_valores(self):
-        numero_1 = None
-        numero_2 = None
-        while numero_1 is None or numero_2 is None:
+        self.numero_1 = None
+        self.numero_2 = None
+        while self.numero_1 is None or self.numero_2 is None:
             try:
-                numero_1 = float(raw_input('ingrese el primer número:'))
-                numero_2 = float(raw_input('ingrese el segundo número:'))
+                self.numero_1 = float(raw_input('ingrese el primer número:'))
+                self.numero_2 = float(raw_input('ingrese el segundo número:'))
             except ValueError:
                 print 'Debe ingresar sólo números. Use punto para los decimales.'
-        return numero_1, numero_2
 
     """
     Este método recibe dos parametros y devuelve el producto de ambos.
     Precondición: a y b deben ser numeros.
     """
-    def multiplicacion(self, numero_1, numero_2):
-        return numero_1 * numero_2
+    def multiplicacion(self):
+        resultado = self.numero_1 * self.numero_2
+        return resultado
 
 """
 Este if sirve para distinguir a cuando se llama el archivo desde otro archivo
